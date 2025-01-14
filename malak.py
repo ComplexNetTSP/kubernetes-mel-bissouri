e1e92e7ff4ac3fea736941e27fc6ed02a9630435
Malakel-biss <malak.elbissouri@gmail.com>
2025-01-14 16:37:58 +0100

    Challenge 3 update

--- malak.py
+++ malak.py
@@ -2,31 +2,35 @@ from flask import Flask, request, render_template_string
 from datetime import datetime
 from pymongo import MongoClient
 
-# Configuration de l'application Flask
+
 app = Flask(__name__)
 
-# Configuration de MongoDB avec un nouveau nom de base de données
-client = MongoClient("mongodb://admin:password@mongodb:27017/")
-db = client["database_challenge3"]  # Nouveau nom de la base de données
+mongo_user = os.getenv("MONGO_USERNAME", "admin")  
+mongo_password = os.getenv("MONGO_PASSWORD", "password")
+mongo_host = os.getenv("MONGO_HOST", "mongodb")  
+
+ 
+client = MongoClient(f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}:27017/")  
+db = client["database_challenge3"]  
 collection = db["records"]
 
 @app.route("/")
 def home():
-    # Informations de l'utilisateur et de la requête
+  
     client_ip = request.remote_addr
     current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    # Enregistrement dans la base de données MongoDB
+
     record = {
         "ip_address": client_ip,
         "date": current_datetime
     }
     collection.insert_one(record)
 
-    # Récupérer les 10 derniers enregistrements
+ 
     last_10_records = list(collection.find().sort([('_id', -1)]).limit(10))
 
-    # Affichage des résultats
+  
     return render_template_string("""
     <h1>Bienvenue sur mon application Flask - Version V2</h1>
     <ul>
